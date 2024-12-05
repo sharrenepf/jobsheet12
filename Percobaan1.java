09861ee392ff9d3ffcbaf0f4eee311c7a26128d7
Sharren <sharrenepf@gmail.com>
2024-12-05 12:35:14 +0700

    percoban1

--- Percobaan1.java
+++ Percobaan1.java
@@ -1,22 +1,24 @@
-public class Percobaan1  {
-    
-    static int faktorialReksrusif(int n) {
+public class Percobaan1 {
+
+    public static void main(String[] args) {
+        System.out.println(faktorialRekursif(5));
+        System.out.println(faktorialIteratif(5));
+    }
+
+    static int faktorialRekursif(int n) {
         if (n == 0) {
             return (1);
         } else {
-            return (n * faktorialReksrusif(n-1));
+            return (n * faktorialRekursif(n - 1));
         }
     }
 
-    static  int faktorialIteratif(int n) {
+    static int faktorialIteratif(int n) {
         int faktor = 1;
         for (int i = n; i >= 1; i--) {
             faktor = faktor * i;
         }
         return faktor;
     }
-    public static void main(String[] args) {
-        System.out.println(faktorialReksrusif(5));
-        System.out.println(faktorialIteratif(5));
-    }
+
 }
